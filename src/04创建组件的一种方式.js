f2217f3d64647955acf9835de6e3d076eb62e94a
duzhuo <1638388435@qq.com>
2020-08-10 12:38:04 +0800

    将组件分离为单独的jsx

--- src/04创建组件的一种方式.js
+++ src/04创建组件的一种方式.js
@@ -2,10 +2,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-//导入Hello文件
-//默认，如果不做单独配置的话，不能省略.jsx
-import Hello from './components/Hello.jsx'
-
+//第一种创建组件的方式
+function Hello(props){//必须大写，否则浏览器会把他当做一个普通的标签来渲染，该DOM元素就失去了它的意义
+    //如果在一个组件中，return一个null，则表示此组件什么都不渲染
+    //return null
+    //必须返回一个合法的jsx
+    ///props.name = 'zs'
+    console.log(props)//形参
+    //不论是Vue还是React，组件中的props永远都只能是只读的，不能被重新赋值
+return <div>这是Hello组件 --- {props.name} --- {props.age} --- {props.gender}</div>//可以返回一个虚拟DOM
+}
 
 const dog = {
     name:'大黄',
---
+++ src/components/Hello.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//第一种创建组件的方式
+export default function Hello(props){//必须大写，否则浏览器会把他当做一个普通的标签来渲染，该DOM元素就失去了它的意义
+    //如果在一个组件中，return一个null，则表示此组件什么都不渲染
+    //return null
+    //必须返回一个合法的jsx
+    ///props.name = 'zs'
+    console.log(props)//形参
+    //不论是Vue还是React，组件中的props永远都只能是只读的，不能被重新赋值
+return <div>这是Hello组件 --- {props.name} --- {props.age} --- {props.gender}</div>//可以返回一个虚拟DOM
+}
+
+//把组件暴露出去
+//export default Hello
