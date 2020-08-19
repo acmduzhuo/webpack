e8434c89d49e094a1eed9520b290ed14df14bddf
duzhuo <1638388435@qq.com>
2020-08-19 22:05:57 +0800

    add 行内样式

--- src/components/CmtItem.jsx
+++ src/components/CmtItem.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 //使用 function 构造函数，定义函数评论项子组件
 export default function CmtItem(props){
-    return <div>
-    <h1>评论人： {props.user}</h1>
-    <p>评论内容：{props.content}</p>
+    return <div style = {{border:'1px dashed #ccc', margin:'10px', padding:'10px', boxShadow:'0 0 10px #ccc'}}>
+    <h1 style={{fontSize:'14px'}}>评论人： {props.user}</h1>
+    <p style={{fontSize:'12px'}}>评论内容：{props.content}</p>
     </div>   
 }
--- src/index.js
+++ src/index.js
@@ -10,7 +10,10 @@ import CmtList from './components/CmtList'
 
 
 ReactDOM.render(<div>
-<h1>这是评论区列表组件</h1>
+    {/*在jsx中，不允许通过style设置字符串的值 来设置 行内样式*/}
+    {/*示例：style = {{color : 'red}} */}
+    {/*在 行内样式 中，数值类型的样式不用单引号，字符型的必须单引号 */}
+<h1 style = {{color : 'red', fontSize: '35px', zIndex: 3, fontWeight : 200, textAlign: 'center'}}>这是评论区列表组件</h1>
 
 <CmtList></CmtList>
 </div>, document.getElementById('app'))
