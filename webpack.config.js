72cbd06e47aa8ebda2f62d45e3ac1167dc1701b4
duzhuo <1638388435@qq.com>
2020-09-06 08:58:21 +0800

    fix error

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
 
             //大家可以在CSS-loader之后，通过？增加参数，
             //其中有一个固定的参数，modules，表示为普通的CSS样式表，启用模块化
-            {test: /\.css$/, use: ['style-loader', 'css-loader?modules&localIdentName=[path]']},//打包处理CSS样式表的第三方loader,从右到左调用，只有CSS-loader才有
+            {test: /\.css$/, use: ['style-loader', { loader: 'css-loader',options: {modules: { localIdentName: '[path][name]-[local]-[hash:5]'}}}]},//打包处理CSS样式表的第三方loader,从右到左调用，只有CSS-loader才有
         ]
     },
     resolve:{
