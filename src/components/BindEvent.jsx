2d4c6b00a17d233bf8cac77865524e388721b806
duzhuo <1638388435@qq.com>
2020-09-10 11:12:37 +0800

    add 事件绑定&修改state

--- src/components/BindEvent.jsx
+++ src/components/BindEvent.jsx
@@ -4,7 +4,9 @@ export default class BindEvent extends React.Component{
     constructor(){
         super()
         //私有数据
-        this.state = {}
+        this.state = {
+            msg:'嘿嘿'
+        }
     }
     render(){
         return<div>
@@ -12,10 +14,30 @@ export default class BindEvent extends React.Component{
             <hr/>
             {/* react有一套自己的事件绑定机制，事件名是小驼峰命名 */}
             {/* <button onClick={function() { console.log('Hello') } }>按钮</button> */}
-            <button onClick={this.myclickHandler}>按钮</button>
+            {/* <button onClick={this.myclickHandler}>按钮</button> */}
+
+            {/* 注意 onClick 只接受function 作为处理函数 */}
+            {/* 注意 箭头函数本身就是一个匿名的函数 */}
+
+            {/* <button onClick={ 
+                ()=>this.show('Hello, World', ' JS')
+            }>按钮</button> */}
+            <button onClick = {()=>this.show('Hello,', 'World!')}
+            >按钮啊</button>
+            <h3>{this.state.msg}</h3>
         </div>
     }
-    myclickHandler(){
-        console.log(2);
+    show = (item1, item2) => {
+        console.log('show方法已被调用了'+item1+item2);
+
+        // console.log(this)
+        // this.state.msg = '哈哈'
+        //在React中，如果想为state赋值，不能用上述方法，应该：
+        this.setState({
+            msg: 'haha ' + item1 + item2
+        })
     }
+    // show (){
+    //     console.log("aaa");
+    // }
 }
