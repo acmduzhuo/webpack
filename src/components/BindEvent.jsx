33d7f74b4c1b9f342a6d7d8c736882e3fb8af615
duzhuo <1638388435@qq.com>
2020-09-11 10:29:01 +0800

    add setState

--- src/components/BindEvent.jsx
+++ src/components/BindEvent.jsx
@@ -5,7 +5,10 @@ export default class BindEvent extends React.Component{
         super()
         //私有数据
         this.state = {
-            msg:'嘿嘿'
+            msg:'嘿嘿',
+            name : 'zs',
+            age : 22,
+            gender : '男'
         }
     }
     render(){
@@ -18,7 +21,7 @@ export default class BindEvent extends React.Component{
 
             {/* 注意 onClick 只接受function 作为处理函数 */}
             {/* 注意 箭头函数本身就是一个匿名的函数 */}
-
+            {/* <button onClick={()=>{this.myclickHandler()}}>按钮</button>  */}
             {/* <button onClick={ 
                 ()=>this.show('Hello, World', ' JS')
             }>按钮</button> */}
@@ -27,6 +30,9 @@ export default class BindEvent extends React.Component{
             <h3>{this.state.msg}</h3>
         </div>
     }
+    // myclickHandler(){
+    //     console.log("Hello World!");
+    // }
     show = (item1, item2) => {
         console.log('show方法已被调用了'+item1+item2);
 
@@ -34,9 +40,19 @@ export default class BindEvent extends React.Component{
         // this.state.msg = '哈哈'
         //在React中，如果想为state赋值，不能用上述方法，应该：
         this.setState({
-            msg: 'haha ' + item1 + item2
+
+            // z在setState，只会把对应的state状态更新，而不会覆盖其他的state状态
+            msg: 'haha ' + item1 + item2,
+            name : '李四'
+        }, function(){
+            console.log(this.state.msg);
         })
+
+        //console.log(this.state.msg);
+        //异步执行，更新未完毕，便执行了输出
+        //需要使用this.setState({}, callback)回调函数
     }
+
     // show (){
     //     console.log("aaa");
     // }
